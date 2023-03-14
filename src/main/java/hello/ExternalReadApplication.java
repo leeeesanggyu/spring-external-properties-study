bb796b62055598e6deb4d85d7e1fae307c4c3991
salgu1998 <salgu1998@gmail.com>
2023-03-14 22:01:06 +0900

    외부 설정 사용 - @ConfigurationProperties를 @ConfigurationPropertiesScan로 빈 등록

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -5,11 +5,13 @@ import hello.config.MyDataSourceValueConfig;
 import hello.datasource.MyDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceValueConfig.class)
 @Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
+@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
